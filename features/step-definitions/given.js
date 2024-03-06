aa9eecf0a62c7861a35e3a23bfe981e0c954ca91
arina24 <dymova.arina@mail.ru>
2024-03-06 10:22:28 -0500

    actions and assertions

--- features/step-definitions/given.js
+++ features/step-definitions/given.js
@@ -1,4 +1,5 @@
 import {Given} from '@cucumber/cucumber';
+import goToUrl from "../../support/actions/goToUrl";
 Given("A Web browser at the Google search page", () => {
-    browser.url("/");
+  goToUrl("/");
   });
--- features/step-definitions/then.js
+++ features/step-definitions/then.js
@@ -1,18 +1,9 @@
 import { Then } from '@cucumber/cucumber';
-import assert from "assert";
+import verifyLinksContain from "../../support/assertions/verifyLinksContain";
 
 Then(/^Links related to "(.*)" are shown on the results page$/, async (keyword)=> {
-  const links = $$(".LC20lb");
-  links.forEach(link => {
-    const linkText = link.getText().toLowerCase();
-
-    if (linkText) {
-      assert(
-        linkText.includes(keyword),
-        `Link text does not include ${keyword}`
-      );
-    }
-  });
+    const links = $$((".LC20lb"));
+    verifyLinksContain(links, keyword);
 });
 
 
